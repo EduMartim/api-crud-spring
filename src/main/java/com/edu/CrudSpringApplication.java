dbb191252d47ffff558144f8b9353f022bbbf069
edumartim <edumartim@gmail.com>
2023-03-16 09:55:20 -0300

    Loading CRUD

---
+++ src/main/java/com/edu/CrudSpringApplication.java
@@ -0,0 +1,31 @@
+package com.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.edu.model.Course;
+import com.edu.repository.CourseRepository;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+    CommandLineRunner initDatabase(CourseRepository courseRepository) {
+        return args -> {
+            courseRepository.deleteAll();
+
+            Course c = new Course();
+            c.setName("Angular com Spring");
+            c.setCategory("front-end");
+
+            courseRepository.save(c);
+        };
+    }
+
+}
---
+++ src/main/java/com/edu/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.edu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edu.model.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
